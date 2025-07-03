13b21c90bbc675811c061ed0db4a0e5d3af84404
SKRR6773 <veerapatzzz846@gmail.com>
2025-07-03 12:15:52 +0700

    TEST winpty

--- src/tests/shell_remote_win.py
+++ src/tests/shell_remote_win.py
@@ -1,7 +1,1 @@
-import subprocess
-
-proc = subprocess.Popen(['cmd.exe', '/k'], shell=True, stdin=subprocess.PIPE, text=True, bufsize=1)
-
-print("Hello CMD ")
-
-proc.terminate()
+from winpy import PtyProcess
