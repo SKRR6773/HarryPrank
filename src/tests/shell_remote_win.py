9d994a6828f05950ba547f88ba8c9525987e086e
SKRR6773 <veerapatzzz846@gmail.com>
2025-07-03 10:58:39 +0700

    TEST UPDATE

--- src/tests/shell_remote_win.py
+++ src/tests/shell_remote_win.py
@@ -1,6 +1,6 @@
 import subprocess
 
-proc = subprocess.Popen(['cmd.exe', '/k'], stdin=subprocess.PIPE, text=True, bufsize=1)
+proc = subprocess.Popen(['cmd.exe', '/k'], shell=True, stdin=subprocess.PIPE, text=True, bufsize=1)
 
 print("Hello CMD ")
 
